b114ba19959a32753c0cda6612b9a115ef835ff4
NitricXd <92805675+NitricXd@users.noreply.github.com>
2022-05-07 09:50:27 +0530

    Update mongo.py

--- MissLyraRobot/mongo.py
+++ MissLyraRobot/mongo.py
@@ -33,7 +33,6 @@ MONGO_DB = "lyra"
 mongodb = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["lyra"]
 
 engine = AIOEngine(motor, MONGO_DB)
 
