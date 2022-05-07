598971878a85fc2d6872f14b6a855e58f9087f19
NitricXd <92805675+NitricXd@users.noreply.github.com>
2022-05-07 07:13:17 +0530

    Update mongo.py

--- MissLyraRobot/services/mongo.py
+++ MissLyraRobot/services/mongo.py
@@ -1,40 +1,22 @@
-# This file is part of Daisy (Telegram Bot)
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import asyncio
 import sys
-
 from motor import motor_asyncio
-from odmantic import AIOEngine
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
+from MissLyraRobot import MONGO_DB_URI
+from MissLyraRobot.confing import get_int_key, get_str_key
 
-from MissLyraRobot import log
-from MissLyraRobot.config import get_int_key, get_str_key
 
-MONGO_URI = get_str_key("MONGO_URI")
-MONGO_PORT = get_int_key("MONGO_PORT")
-MONGO_DB = get_str_key("MONGO_DB")
+MONGO_PORT = get_int_key("27017")
+MONGO_DB_URI = get_str_key("MONGO_DB_URI")
+MONGO_DB = "MissLyraRobot"
 
-# Init MongoDB
-mongodb = MongoClient(MONGO_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_URI, MONGO_PORT)
+
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-
-engine = AIOEngine(motor, MONGO_DB)
-
+db = client["MissLyraRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
